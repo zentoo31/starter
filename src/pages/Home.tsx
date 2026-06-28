9fca95a205d6600294e7d49abbd4b27b3384c71c
zentoo31 <zentoo31@gmail.com>
2026-06-28 14:01:10 -0500

    Fetch and display system information in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import type { SystemInfo } from "@/types/systemInfo";
+
 function Home() {
+  const [info, setInfo] = useState<SystemInfo | null>(null);
+  useEffect(() => {
+    async function load(){
+      const datos: SystemInfo = await window.electronAPI.getSystemInfo();
+      setInfo(datos);
+      console.log(datos)
+    }
+    load();
+  }, []);
+
   return (
     <>
       <h1>This is home</h1>
+      <pre>{info?.cpu.brand}</pre>
     </>
   );
 }
