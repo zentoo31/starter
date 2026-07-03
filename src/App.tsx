9486327df4f3ad6c8f7f79140681ea1b5232835e
zentoo31 <zentoo31@gmail.com>
2026-07-03 14:17:08 -0500

    Add route for Develop page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Programs from "./pages/Programs";
 import Layout from "./components/Layout";
+import Develop from "./pages/Develop";
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/programs" element={<Programs />} />
+        <Route path="/develop" element={<Develop />} />
       </Routes>
     </Layout>
   );
