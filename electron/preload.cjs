a66b73240ef8a281176ebbd3537647b2b7fd3af9
zentoo31 <zentoo31@gmail.com>
2026-07-03 14:15:52 -0500

    Add IPC methods for program management and logging in preload script

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,4 +4,17 @@ console.log("PRELOAD FUNCIONA");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   getSystemInfo: () => ipcRenderer.invoke("system:getInfo"),
+  checkWinget: () => ipcRenderer.invoke("winget:check"),
+  checkProgramInstalled: (program) => ipcRenderer.invoke("program:check-installed", program),
+  installProgram: (program) => ipcRenderer.invoke("program:install", program),
+  uninstallProgram: (program) => ipcRenderer.invoke("program:uninstall", program),
+  onInstallLog: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+
+    ipcRenderer.on("install:log", listener);
+
+    return () => {
+      ipcRenderer.removeListener("install:log", listener);
+    };
+  },
 });
