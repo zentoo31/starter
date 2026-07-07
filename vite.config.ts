887de4bf6eb98a5b290b1033f7d317dc8d521665
zentoo31 <zentoo31@gmail.com>
2026-07-07 00:11:42 -0500

    Fix formatting in vite.config.ts and ensure newline at end of file

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
