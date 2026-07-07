c4efe1215001b070d3927c7534bfe3ff7d4ebcd7
zentoo31 <zentoo31@gmail.com>
2026-07-06 23:38:01 -0500

    Add functions to open Ninite download and execute Massgrave in electronAPI

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -6,8 +6,10 @@ contextBridge.exposeInMainWorld("electronAPI", {
   getSystemInfo: () => ipcRenderer.invoke("system:getInfo"),
   checkWinget: () => ipcRenderer.invoke("winget:check"),
   checkProgramInstalled: (program) => ipcRenderer.invoke("program:check-installed", program),
+  openNiniteDownload: (selectedIds) => ipcRenderer.invoke("program:open-ninite", selectedIds),
   installProgram: (program) => ipcRenderer.invoke("program:install", program),
   uninstallProgram: (program) => ipcRenderer.invoke("program:uninstall", program),
+  executeMassgrave: () => ipcRenderer.invoke("windows:execute-massgrave"),
   onInstallLog: (callback) => {
     const listener = (_event, payload) => callback(payload);
 
