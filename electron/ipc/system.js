09917b45020a0413eed1895ae70d50e05cfacdf9
zentoo31 <zentoo31@gmail.com>
2026-07-07 23:56:17 -0500

    Add network interfaces data to system info retrieval

--- electron/ipc/system.js
+++ electron/ipc/system.js
@@ -4,15 +4,15 @@ import si from "systeminformation";
 ipcMain.handle("system:getInfo", async () => {
     console.log("entro al ipc")
     console.log("ipc called")
-    const [cpu, mem, graphics, os, disks] = await Promise.all([
+    const [cpu, mem, graphics, os, disks, networkInterfaces] = await Promise.all([
         si.cpu(),
         si.mem(),
         si.graphics(),
         si.osInfo(),
-        si.fsSize()
+        si.fsSize(),
+        si.networkInterfaces()
     ]);
-
     return {
-        cpu, mem, graphics, os, disks
+        cpu, mem, graphics, os, disks, networkInterfaces
     }
 })
