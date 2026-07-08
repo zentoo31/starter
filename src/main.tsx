cce3fbf7d7713c8eeab6297122f790985092ed03
zentoo31 <zentoo31@gmail.com>
2026-07-08 00:08:59 -0500

    Replace BrowserRouter with HashRouter for improved routing consistency

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import "./index.css";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { SystemProvider } from "./context/SystemContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <SystemProvider>
       <App />
     </SystemProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
